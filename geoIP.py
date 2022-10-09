e9abe7601a81c0ab7d698923d99e8bc621ca49f6
Gianluca Cometa <gianluca.antonio31@gmail.com>
2022-10-09 13:13:49 +0200

    add plot function and dependecies

--- geoIP.py
+++ geoIP.py
@@ -1,3 +1,4 @@
+####GIANLUCA COMETA - CISIA#####
 import argparse
 import contextlib
 import json
@@ -5,8 +6,54 @@ import sys
 from requests import Session
 from requests_ratelimiter import LimiterSession
 
+###FROM GEOIPPlotter###
+# Imports
+import argparse
+import contextlib
+import sys
+import matplotlib
+import numpy as np
+# Anti-Grain Geometry (AGG) backend so PyGeoIpMap can be used 'headless'
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib import cm
+from numpy import array
+import matplotlib.colors as colors
+
+# Adopted from https://stackoverflow.com/questions/10664856/make-a-dictionary-with-duplicate-keys-in-python
+# This class will allow us to store multiple iterations of the samle lat/lon for specific IP so we can establish weight
+class DictList(dict):
+    def __setitem__(self, key, value):
+        try:
+            # Assumes there is a list on the key
+            self[key].append(value)
+        except KeyError: # If it fails, because there is no key
+            super(DictList, self).__setitem__(key, value)
+        except AttributeError: # If it fails because it is not a list
+            super(DictList, self).__setitem__(key, [self[key], value])
+
+
+#lon/lat dictionary for some plot types
+long_lat = dict()
+long_lat2=DictList()
+
+def get_marker_color(size):
+    if size < 1:
+        return 'b'#blue
+    if size > 2 and size < 3:
+        return 'g' # green
+    if size > 3 and size < 4:
+        return 'c'
+    if size > 4 and size < 5:
+        return 'm'
+    if size > 5:
+        return 'r' #red
+
+
+
 from requests import get
-session = LimiterSession(per_minute=1)
+session = LimiterSession(per_second=10)
 
 def get_ip(ip_file):
     """
@@ -24,6 +71,8 @@ def geo(ip_list=[]):
             data = loc.json()
             print(data)
             print(data['country'])
+            print(data['lat'])
+            print(data['lon'])
         except Exception:
             print("Unable to process IP: %s" % ip)
             continue
